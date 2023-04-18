b1bfb87f068084cef60627a2d0d1d9eddd91fb8b
gr411_vokan <forstuding411@gmail.com>
2023-04-18 15:25:52 +0700

    2

--- school/app/models.py
+++ school/app/models.py
@@ -16,3 +16,21 @@ class DZ(models.Model):
     title = models.CharField(max_length=255)
     number = models.IntegerField
     file = models.FilePathField
+    teacher_comment = models.TextField
+    student_comment = models.TextField
+
+
+class Sub(models.Model):
+    id = models.AutoField
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class TeacherUser(models.Model):
+    name = models.CharField(max_length=155)
+    subject = models.ForeignKey(Sub, on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        permissions = ()
